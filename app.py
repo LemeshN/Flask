3c58b3f6b647d954ff4898362f73ead5c2aa13ab
LemeshN <lemesh.mikalai@gmail.com>
2022-08-10 16:55:56 +0300

    flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,28 +16,32 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
-        return '<Article %r' %self.id
+        return '<Article %r' % self.id
+
 
 @app.route('/')
-@app.route('/home')
 def main():
     return render_template('main.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
 
+
 @app.route('/posts/<int:id>')
 def posts_detail(id):
     article = Article.query.get(id)
     return render_template('posts_detail.html', article=article)
 
-@app.route('/posts/<int:id>/del')
+
+@app.route('/posts/<int:id>/delete')
 def post_delete(id):
     article = Article.query.get_or_404(id)
 
@@ -47,20 +52,16 @@ def post_delete(id):
     except:
         return "При удалении статьи произошла ошибка"
 
-@app.route('/123')
-def onetwothree():
-    return render_template('123.html')
 
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
 def posts_update(id):
+    article = Article.query.get(id)
     if request.method == "POST":
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-        article = Article(title=title, intro=intro, text=text)
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
 
         try:
-            db.session.add(article)
             db.session.commit()
             return redirect('/posts')
         except:
@@ -69,6 +70,7 @@ def posts_update(id):
     else:
         article = Article.query.get(id)
         return render_template('posts_update.html', article=article)
+
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
@@ -87,6 +89,7 @@ def create():
 
     else:
         return render_template('create.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
